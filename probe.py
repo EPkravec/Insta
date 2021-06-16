c692942371de223e6c190ed45aa6680a09936ff3
EPkravec <kravec.egor@gmail.com>
2021-06-17 00:51:48 +0400

    логика написана нужно править  ссылки  + написать коменты

--- probe.py
+++ probe.py
@@ -1,16 +1,12 @@
-
-
 import random
 import time
 from time import sleep
 from selenium import webdriver
 from multiprocessing import Process
 
-
-from configLPT import my_tags
+from configLPT import my_tags, my_coments
 
 pars_chek = False  # переменая для контроля спарсеных данный с сайта проверки настроек браузера на ликвидность
-
 
 
 class LogicInstagramm:
@@ -22,12 +18,14 @@ class LogicInstagramm:
         self.login: str = login
         self.password: str = password
         self.tags_defaul: list = my_tags
+        self.coments_defaul: list = my_coments
         self.url_instagramm: str = 'https://www.instagram.com/'
         self.url_check: str = 'https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'
         self.time_sleep_login: int = random.randint(5, 8)
         self.time_sleep: int = random.randint(20, 30)
         self.time_sleep_scroll: int = random.randint(10, 12)
         self.count_tags: int = 1
+        self.count_coments: int = 1
         self.ignore_link: int = 9
         self.repeat_link: bool = False
         self.max_like_day: int = 115
@@ -35,17 +33,15 @@ class LogicInstagramm:
         self.min_time_wait_for_like = (60 // self.max_like_hour) * 60
         self.time_sleep_like: int = random.randint(self.min_time_wait_for_like - 20, self.min_time_wait_for_like + 20)
         self.count_scroll: int = 1  # 12 шт за один скролл в теории
-        self.datatime: str = self.check_time()
+        self.datatime = self.datatime()
 
-    def time1(self):
-        a = time.localtime()
-        data = time.strftime("%Y-%m-%d %H:%M:%S", a)
-        return data
-
-    def check_time(self):
+    def datatime(self):
+        """
+        Ф-я отображения времени ( не работате ((( )))
+        :return:
+        """
         while True:
-            self.time1()
-            return self.time1()
+            return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     def options_argument(self):
         """
@@ -103,10 +99,20 @@ class LogicInstagramm:
         pars_chek = False
         return pars_chek
 
+    def coments(self):
+        """
+        Ф-я определения комментариев из основного списка, зависит от настройки self.count_coments по умолчанию 1
+        :return: список комментариев
+        """
+        random.shuffle(self.coments_defaul)
+        my_coments = self.coments_defaul[:self.count_coments]
+        my_coments = ''.join(my_coments)
+        return my_coments
+
     def tags(self):
         """
         Ф-я определения тегов из основного списка, зависит от настройки self.count_tags по умолчанию 1
-        :return:
+        :return: список тегов
         """
         random.shuffle(self.tags_defaul)
         my_hashtags = self.tags_defaul[:self.count_tags]
@@ -147,6 +153,7 @@ class LogicInstagramm:
         sleep(self.time_sleep_login)
 
         my_hashtags = self.tags()
+        my_coments = self.coments()
 
         print(f'{self.datatime} [{self.login}] - Ищем фоточки по тегу {my_hashtags}')
         browser.get(f'https://www.instagram.com/explore/tags/{my_hashtags}/')
@@ -166,47 +173,119 @@ class LogicInstagramm:
                     post_urls.append(href)
 
             if len(post_urls) > 50:  # todo  времнное значение, больше 54 ссылок не могу спарсить
-                count_scroll = self.count_scroll
-                self.count_scroll = count_scroll
                 print(
                     f'{self.datatime} [{self.login}] - {self.max_like_hour} лайка в час, Ссылок для работы - {len(post_urls)}')
-                del post_urls[0:self.ignore_link]
                 poisk = False
             else:
                 continue
 
-        i = 0  # часовой счетчик лайка
-        j = 0  # суточный счетчик лайка
-        k = 0  # общий счетчик лайка
+        lik1 = 0  # счетчик лайка
+        com1 = 0  # счетчик комментариев
+        flow1 = 0  # счетчик подписок
 
         for url in post_urls:
             browser.get(url)
             sleep(self.time_sleep)
+            # заходим на фото
             browser.find_element_by_xpath(
-                '/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
-            i += 1
-            j += 1
-            k += 1
-            print(
-                f'{self.datatime} [{self.login}] - лайк за час [{i}/{self.max_like_hour}] за сутки [{k}/{self.max_like_day}]')
-            print(f'{self.datatime} [{self.login}] - Лайк по тегу [{my_hashtags}] адрес - {url}')
-            sleep(self.time_sleep_like)
-            # todo сделать условие проверки наличия ссылок для лайков, переходов и прочее
-            if i >= self.max_like_hour:
-                print(f'{self.datatime} [{self.login}] - Дастигнут часовой лимит лайков [{i}/{self.max_like_hour}]')
-                i = 0
-                sleep(self.time_sleep_like * 2)
-                continue
-            elif i >= self.max_like_day:
-                print(f'{self.datatime} [{self.login}] - Дастигнут суточный лимит лайков [{k}/{self.max_like_day}]')
-                j = 0
-                sleep(self.time_sleep_like)
-                print(f'{self.datatime} [{self.login}] - Пора отдохнуть или придумать новые настройки')
-                browser.close()
-                browser.quit()
-                break
-            else:
-                continue
+                '/html/body/div[1]/section/main/div/div[1]/article/header/div[2]/div[1]/div').click()
+            sleep(self.time_sleep)
+            # заходим на акаутн
+            browser.find_element_by_xpath(
+                '/html/body/div[1]/section/main/div/div[1]/article/header/div[2]/div[1]/div/span/a').click()
+            sleep(self.time_sleep)
+
+            # заходим на фото в акауте
+            browser.find_element_by_xpath(
+                '/html/body/div[1]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a').click()
+
+            sleep(self.time_sleep)
+            # заходим на фото в акауте
+            # browser.get(post_urls_top_ac)
+            # sleep(self.time_sleep)
+            # # смотрим количество лайковна фото в акауте
+            # count_like_button = browser.find_element_by_xpath(
+            #     '/html/body/div[5]/div[2]/div/article/div[3]/section[2]/div/div/a/span')
+            # sleep(self.time_sleep)
+            # count_like = count_like_button.text
+            # count_like = int(count_like.split(' ')[0])
+            # print(f'{self.datatime} [{self.login}] - Количество лайков под фото - {count_like}')
+            # loop_count = int(count_like / 12)
+            # # смотрим список людей которые лайкнули фото в акауте
+            # count_like_button.click()
+            # sleep(self.time_sleep)
+            # # ищем всех кто лайкнул
+            # like_pipl = browser.find_element_by_xpath('/html/body/div[5]/div/div/div[2]')
+            # sleep(self.time_sleep)
+            # like_pipl_urls = []
+            #
+            # for i in range(1, loop_count + 1):
+            #     browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", like_pipl)
+            #     sleep(self.time_sleep_scroll)
+            #
+            # all_hrefs = like_pipl.find_elements_by_tag_name('a')
+            #
+            # for url in all_hrefs:
+            #     url = url.get_attribute('href')
+            #     like_pipl_urls.append(url)
+            #
+            # like_pipl_urls = set(like_pipl_urls)
+            # like_pipl_urls = list(like_pipl_urls)
+            #
+            # for pipl in like_pipl_urls:
+            #     print(f'{self.datatime} [{self.login}] - Заходим к - {pipl}')
+            #     # заходим на чеорвека который лайкнул
+            #     browser.get(f'https://www.instagram.com/{pipl}/')
+            #     sleep(self.time_sleep_scroll)
+            #     # проверяем закрыт ли аккаунт
+            #     close_ac = browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div/article/div[1]/div/h2')
+            #     sleep(self.time_sleep)
+            #     count_like = close_ac.text
+            #     if 'Это закрытый аккаунт' == count_like:
+            #         continue
+            #     # подписываемся
+            #     browser.find_element_by_xpath(
+            #         '/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button').click()
+            #     flow1 += 1
+            #     sleep(self.time_sleep)
+            #     # ставим комент
+            #     browser.find_element_by_xpath(
+            #         '/html/body/div[1]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a').click()
+            #     sleep(self.time_sleep)
+            #     comment = browser.find_element_by_xpath(
+            #         '/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea').click()
+            #     sleep(self.time_sleep)
+            #     comment.send_keys(my_coments)
+            #     sleep(self.time_sleep)
+            #     browser.find_element_by_xpath(
+            #         '/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button[2]').click()
+            #     com1 += 1
+            #     sleep(self.time_sleep)
+            #     # ставим лайк
+            #     browser.find_element_by_xpath(
+            #         '/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
+            #     lik1 += 1
+            #     sleep(self.time_sleep)
+            #     print(f'{self.datatime} [{self.login}] - Лайк по тегу [{my_hashtags}] адрес - {url}')
+            #
+            #     print(
+            #         f'{self.datatime} [{self.login}] - Лайков за сутки [{lik1}/{self.max_like_day}]')
+            #     print(
+            #         f'{self.datatime} [{self.login}] - Комментариев за сутки [{com1}/{self.max_like_day}]')
+            #     print(
+            #         f'{self.datatime} [{self.login}] - Подписок за сутки [{flow1}/{self.max_like_day}]')
+            #
+            #     sleep(self.time_sleep_like)
+            #
+            #     # todo сделать условие проверки наличия ссылок для лайков, переходов и прочее
+            #     if lik1 >= self.max_like_day and com1>= self.max_like_day and flow1>= self.max_like_day:
+            #         print(f'{self.datatime} [{self.login}] - Дастигнут суточный лимит лайков комментариев и подписок')
+            #         print(f'{self.datatime} [{self.login}] - Завершаю сессию')
+            #         browser.close()
+            #         browser.quit()
+            #         break
+            #     else:
+            #         continue
 
 
 def run(login, password):
@@ -232,8 +311,8 @@ def run(login, password):
 
 
 if __name__ == "__main__":
-    Process(target=run, args=('Rabota_v_dekreteizdoma', '123456789q')).start()
-    sleep(5)
+    # Process(target=run, args=('Rabota_v_dekreteizdoma', '123456789q')).start()
+    # sleep(5)
     Process(target=run, args=('dekret_rabota_olga', '123456789q')).start()
-    sleep(5)
-    Process(target=run, args=('rabo.tavradosti', '123456789qQ')).start()
+    # sleep(5)
+    # Process(target=run, args=('rabo.tavradosti', '123456789qQ')).start()
